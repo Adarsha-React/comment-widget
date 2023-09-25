a46030ae7000665419ce244d3563563d2a7821ad
= <=>
2023-09-25 12:50:25 +0530

    Added Tailwind UI Framework + src folder

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Header from "./src/components/Header";
+import Body from "./src/components/Body";
 
 const AppLayout = () => {
   return (
     <div>
-      <h1>App is running!!</h1>
+      <Header />
+      <Body />
     </div>
   );
 };
