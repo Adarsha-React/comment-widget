f6a88f0cb915d1ff98baea5329bd18e236d20e6d
= <=>
2023-09-25 18:02:43 +0530

    Moved App.js into src folder + verified the content array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
+import Header from "./components/Header";
+import Body from "./components/Body";
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="w-1/2 mx-auto">
       <Header />
       <Body />
     </div>
